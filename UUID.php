b45eed6c44bbc6686da3e0f25939ae318e4cd7b8
Mihail Binev <kodeart@gmail.com>
2017-05-01 09:53:56 +0200

    Cosmetics

--- UUID.php
+++ UUID.php
@@ -192,7 +192,7 @@ final class UUID
             mt_rand(0, 0xffff), mt_rand(0, 0xffff),
             mt_rand(0, 0xffff),
             mt_rand(0, 0x0fff) | 0x4000,
-            mt_rand(0, 0x3fff) | 0x8000, // [8,9,a,b]
+            mt_rand(0, 0x3fff) | 0x8000, // 8,9,a,b
             mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
         );
     }
