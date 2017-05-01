cf570754ff1779405a0bf71c5fc1814ded90e138
Mihail Binev <kodeart@gmail.com>
2017-05-01 09:53:33 +0200

    Adds a new unit test for parse_ini_string() bug which not transform the booleans correctly

---
+++ Tests/BugConfigFromIniStringTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Koded\Stdlib;
+
+use PHPUnit\Framework\TestCase;
+
+class BugConfigFromIniStringTest extends TestCase
+{
+
+    public function test_parse_ini_string_expected_same_transformation_as_from_parse_ini_file()
+    {
+        $config = new Config;
+        $config->fromEnvironment(['KEY_4'], '', false);
+
+        $this->assertNotSame(true, $config->get('KEY_4'), 'Expects (bool)true instead (int)1');
+        $this->assertSame(1, $config->get('KEY_4'), 'parse_ini_string(): booleans are returned as integers...');
+    }
+}
