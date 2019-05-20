a497979cd865c683e964a6d93ef0b0beb9827512
Mihail Binev <kodeart@gmail.com>
2019-05-20 18:14:48 +0200

    - changes the method signature (accepts iterable instead of array)

--- Interfaces.php
+++ Interfaces.php
@@ -21,15 +21,15 @@ interface ArrayDataFilter
      * Strips the portions of the variable names defined by the namespace value.
      * Returns a filtered array.
      *
-     * @param array  $data      The data should be filtered
-     * @param string $prefix    The namespace prefix for the indexes
-     * @param bool   $lowercase [optional] Returned indexes should be in lowercase
-     * @param bool   $trim      [optional] To remove the namespace from the index
+     * @param iterable $data      The data should be filtered
+     * @param string   $prefix    The namespace prefix for the indexes
+     * @param bool     $lowercase [optional] Returned indexes should be in lowercase
+     * @param bool     $trim      [optional] To remove the namespace from the index
      *
      * @return array A filtered array
      */
     public function filter(
-        array $data,
+        iterable $data,
         string $prefix,
         bool $lowercase = true,
         bool $trim = true
@@ -49,11 +49,9 @@ interface ArrayDataFilter
      *  $dataObject->find('foo');     // yields 1
      *  $dataObject->find('bar.baz'); // yields 'gir'
      *
-     * @return array
-     *
      * @param string $index   The name of the property (dot-notation)
      * @param mixed  $default [optional] Default value if item is not found
-     *
+
      * @return mixed
      */
     public function find(string $index, $default = null);
@@ -93,8 +91,8 @@ interface Data
 {
 
     const E_CLONING_DISALLOWED = 1000;
-    const E_READONLY_INSTANCE = 1001;
-    const E_PHP_EXCEPTION = 1002;
+    const E_READONLY_INSTANCE  = 1001;
+    const E_PHP_EXCEPTION      = 1002;
 
     /**
      * Value accessor, gets a value by name.
@@ -364,13 +362,13 @@ interface Configuration extends Data
 interface Serializer
 {
     const E_INVALID_SERIALIZER = 409;
-    const E_MISSING_MODULE = 424;
+    const E_MISSING_MODULE     = 424;
 
     const IGBINARY = 'igbinary';
-    const MSGPACK = 'msgpack';
-    const JSON = 'json';
-    const XML = 'xml';
-    const PHP = 'php';
+    const MSGPACK  = 'msgpack';
+    const JSON     = 'json';
+    const XML      = 'xml';
+    const PHP      = 'php';
 
     /**
      * Creates a serialized representation of the data.
---
+++ Tests/ArrayDataFIlterTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Koded\Stdlib;
+
+use PHPUnit\Framework\TestCase;
+
+class ArrayDataFIlterTest extends TestCase
+{
+
+    /**
+     * @dataProvider iterableData
+     */
+    public function test_iterable_data($data)
+    {
+        $object = new Arguments;
+
+        $this->assertEquals(['key0' => 'val0'], $object->filter($data, 'prefix.'));
+    }
+
+    public function iterableData()
+    {
+        $gen = function() {
+            yield 'prefix.key0' => 'val0';
+        };
+
+        return [
+            [['prefix.key0' => 'val0']],
+            [$gen()],
+        ];
+    }
+}
