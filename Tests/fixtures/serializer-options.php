92c23614e23cb4c63be5a0ec8366d18b47d31068
Mihail Binev <kodeart@gmail.com>
2021-11-21 06:59:00 +0100

    - fixture for JSON serialization

---
+++ Tests/fixtures/serializer-options.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    '1',
+    1,
+    '2.01',
+    2.01,
+    'Bj\u00f6rk Gu\u00f0mundsd\u00f3ttir',
+    'Björk Guðmundsdóttir',
+    'Bj\u00f6rk Guðmundsdóttir',
+    '\//',
+    true,
+    false,
+];
